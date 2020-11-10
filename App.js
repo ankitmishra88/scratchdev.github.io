a41d1eb1cbe12c9406c64136b584a7c62787f005
Ankit Mishra <ankitstudy88@gmail.com>
2020-11-10 18:18:12 +0530

    scratchnest build code

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "styles.css";
+import Nav from "./Components/Navi_Bar";
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+export default App;
